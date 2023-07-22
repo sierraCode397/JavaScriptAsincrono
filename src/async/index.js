d726530d9b1019b3a3fe8b7e13281f0e85bf88c8
izaack107 <izaack107@gmail.com>
2023-07-21 20:31:49 -0600

    Funciones Asincronas

---
+++ src/async/index.js
@@ -0,0 +1,16 @@
+const fnAsync = () => {
+    return new Promise ((resolve, reject) => {
+        (true) 
+        ? setTimeout(() => resolve("Async!!"),2000)
+        : reject(new error("Error!!"));
+    })
+} 
+const anotherFn = async () => {
+    const something = await fnAsync();
+    console.log(something)
+    console.log("Hello")
+}
+
+console.log("Before");
+anotherFn()
+console.log("After");
