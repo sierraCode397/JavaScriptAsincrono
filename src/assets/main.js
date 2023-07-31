224c3cfd05091a09e9768146ba33077374a6b0b5
izaack107 <izaack107@gmail.com>
2023-07-30 19:59:17 -0600

    Deploy de proyecto

--- src/assets/main.js
+++ src/assets/main.js
@@ -24,7 +24,7 @@ async function fetchData(urlApi) {
         <div class="group relative">
             <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
             </div>
             <div class="mt-4 flex justify-between">
             <h3 class="text-sm text-gray-700">
@@ -33,10 +33,11 @@ async function fetchData(urlApi) {
             </h3>
             </div>
         </div>
-        `).slice(0, 6).join('')}
+        `).slice(0, 8).join('')}
 
     `;
+    content.innerHTML = view;
     } catch (error) {
-        console.log(error);
+        alert(error);
     }
 })();
